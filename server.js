83878ffcd11d496e02561fb6ed5e94ac83748b5d
Fernando Herrera <fermh2010@gmail.com>
2016-10-25 16:43:52 +0000

    implemented server logic

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 
-app.all('*', function(req, res) {
-   res.end('test') ;
+app.get('/:time', function(req, res, next) {
+    const raw_time = req.params.time;
+    var date;
+    const unix_time = parseInt(raw_time);
+    if(isNaN(unix_time)) {
+        date = new Date(raw_time);
+    } else {
+        date = new Date(unix_time);
+    }
+    
+    var out = {};
+    if(isNaN(date.getTime())) {
+        out.unix = null;
+        out.natural = null;
+    } else {
+        out.unix = date.getTime();
+        out.natural = date.toUTCString();
+    }
+    
+    res.json(out);
 });
 
 app.listen(8080);
